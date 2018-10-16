a9913ff91c4400e8d913e1473b29dd079b0bd0fb
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-10-15 22:05:35 -0500

    adds styled-components and Button component.

---
+++ src/components/common/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: white;
+  border: 5px solid black;
+  font-size: 20px;
+  color: #212121;1
+`
+
+export default Button;
